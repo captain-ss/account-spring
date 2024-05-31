cc4970eaca5ac38a818b1fd91c4c4961784ce316
Swdadesh Vic <swadeshishu2001@gmail.com>
2024-05-31 20:06:39 +0530

    okay

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // Constructors, getters, setters
+}
