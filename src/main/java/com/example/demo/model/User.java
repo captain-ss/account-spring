6f8f388eee5bb694956ab87db7e0739764be0ccd
Swdadesh Vic <swadeshishu2001@gmail.com>
2024-06-01 12:21:14 +0530

    error unsatisfied

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -16,5 +16,37 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    // Constructors, getters, setters
+    // Constructors
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
