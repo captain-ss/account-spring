4ccc1aab8898071edff4de59c81c40ae412157fc
Quasilius Starlord <joysen833@gmail.com>
2024-06-02 12:22:37 +0530

    Resolved dependency injection failure

--- src/main/java/com/example/demo/AccountApplication.java
+++ src/main/java/com/example/demo/AccountApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.demo.repository.*")
+@ComponentScan(basePackages = { "com.example.demo.model.*" })
+@EntityScan(basePackages = "com.example.demo.model.*")
 public class AccountApplication {
 
     public static void main(String[] args) {
